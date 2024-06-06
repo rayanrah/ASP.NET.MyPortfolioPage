ab6602b55f7dc6d36058ca1cb8a8e755a89dcbbf
ryan.Rah <re.rahdari@gmail.com>
2024-06-06 11:11:52 +0200

    Add project files.

---
+++ WebApplication1/Controllers/PersonalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MY_PORTFOLIO.Models;
+
+namespace MY_PORTFOLIO.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new PersonalVM());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //With the 'Post method' you can send as much as data you want because the Data store in the budy request.  
+        public IActionResult ShowPersonal(PersonalVM personalVM)
+
+        {
+            return View(personalVM);
+        }
+    }
+}
---
+++ WebApplication1/Models/PersonalVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MY_PORTFOLIO.Models
+{
+    public class PersonalVM
+    {
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "NAME")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string SurName { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+    }
+
+}
